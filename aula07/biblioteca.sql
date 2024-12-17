6acaacdae614ac9a3bc4e3832549d47d150e7615
Hatoridev <jonasdev21@outlook.com>
2024-12-17 16:30:59 -0300

    Adicionando arquivos da aula07 de SQL, analisando sua possível atribuição no MySQL

---
+++ aula07/biblioteca.sql
@@ -0,0 +1,3 @@
+SQLite version 3.47.2 2024-12-07 20:39:59
+Enter ".help" for usage hints.
+[?2004hsqlite> [Ksqlite> [Ksqlite> [Ksqlite> [Ksqlite> ^C[?2004l[?2004h[C[C[C[C[C[C[C[C^C[?2004l
